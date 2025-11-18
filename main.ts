7a890ecf5bb78893d96d257acd3381bfc356074d
Tomasz Szepczynski <therepo90@gmail.com>
2025-11-18 22:44:20 +0100

    ig

--- main.ts
+++ main.ts
@@ -2,25 +2,17 @@ import './styles.less';
 import * as BABYLON from 'babylonjs';
 import * as bgShader from './assets/bg.shader.fragment.fx';
 
-
-//console.log('Kod szejdera', {bgShader}) // potrzebne bo wtedy mysli ze jest potrzebne lawl.
-
-async function loadShader(url) {
-    const response = await fetch(url);
-    return await response.text();
-}
-
-var canvas;
 function getResolution(scene){
     var c = scene.getEngine().getRenderingCanvas()
     return new BABYLON.Vector2(c.width, c.height)
 }
 
-const createBgPostProcess = (scene: BABYLON.Scene, camera: BABYLON.Camera) => {
+const createBgPostProcess = (scene: BABYLON.Scene, camera: BABYLON.Camera, cfg) => {
+    const canvas = scene.getEngine().getRenderingCanvas();
     BABYLON.Effect.ShadersStore["bgFragmentShader"] = bgShader;
     var postProcess = new BABYLON.PostProcess("bg", "bg", ["iResolution", "iTime", "iMouse", 'laserTint'], ["iChannel0"], 1.0, camera);
     postProcess.alphaMode = BABYLON.Engine.ALPHA_COMBINE;
-    var iChannel0 = new BABYLON.Texture('fb.png', scene);
+    var iChannel0 = new BABYLON.Texture(cfg.img, scene);
     let mousePos = new BABYLON.Vector2(0,0); // left-bottom of canvas is (0,0)
     document.addEventListener('mousemove', function(e) {
             mousePos = new BABYLON.Vector2( (e.pageX - canvas.offsetLeft) / canvas.width, 1.0 - (e.pageY - canvas.offsetTop) / canvas.height);
@@ -35,13 +27,12 @@ const createBgPostProcess = (scene: BABYLON.Scene, camera: BABYLON.Camera) => {
         effect.setVector2('iResolution', getResolution(scene))
         effect.setFloat('iTime', time)
         effect.setVector2('iMouse', mousePos);
-        effect.setVector3('laserTint', new BABYLON.Vector3(0.043, 0.573, 0.969));
+        effect.setVector3('laserTint', cfg.borderColor);
     }
     return postProcess;
 };
 
-window.addEventListener('DOMContentLoaded', function () {
-    canvas = document.getElementById('renderCanvas') as any;
+function initCanvasEngine(canvas: any, cfg: any) {
     var engine = new BABYLON.Engine(canvas, true);
 
     var createScene = function () {
@@ -49,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function () {
         scene.clearColor = new BABYLON.Color4(0.0, 0, 0, 0);
         var camera = new BABYLON.UniversalCamera('UniversalCamera', new BABYLON.Vector3(0, 0, -1), scene);
         camera.setTarget(BABYLON.Vector3.Zero());
-        const pgPostProcess = createBgPostProcess(scene, camera);
+        const pgPostProcess = createBgPostProcess(scene, camera, cfg);
 
         return scene;
     };
@@ -65,4 +56,19 @@ window.addEventListener('DOMContentLoaded', function () {
     setTimeout(() => {
         engine.resize();
     }, 50); // some fancy bug;
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    const canvas = document.getElementById('renderCanvas') as any;
+    const canvas2 = document.getElementById('renderCanvas2') as any;
+    const fbCfg = {
+        img: 'fb.png',
+        borderColor: new BABYLON.Vector3(0.043, 0.573, 0.969)
+    }
+    const instaCfg = {
+        img: 'insta.png',
+        borderColor: new BABYLON.Vector3(254,0,129)
+    }
+    initCanvasEngine(canvas, fbCfg);
+    initCanvasEngine(canvas2, instaCfg);
 });
