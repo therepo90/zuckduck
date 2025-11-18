0ed0a586680682baf5c93c65612d463e8cfa6af4
Tomasz Szepczynski <therepo90@gmail.com>
2025-11-19 00:38:21 +0100

    x

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 // @ts-ignore
-export const api = process.env.LOCAL_DEV === 'true' ? 'http://localhost:3000' : 'https://deletesocialmedia.idontknowhatimdoing.com/';
+export const api = process.env.LOCAL_DEV === 'true' ? 'http://localhost:3000' : 'https://apideletesocialmedia.idontknowhatimdoing.com';
